cf55f330f823709505c11dbe446b77c7fd7a3d9e
MoatasemGamal <moatasemco@gmail.com>
2024-10-14 00:20:24 +0300

    add theme colors, post styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./main.js"],
+	darkMode: 'class',
+	content: ["./*.html", "./main.js"],
 	theme: {
 		extend: {
 			fontFamily: {
 				sans: ["Space Grotesk", "IBM Plex Sans Arabic", "sans-serif"],
+			},
+			colors: {
+				background: "rgba(var(--clr-background))",
+				"txt-primary": "rgba(var(--clr-txt-primary))",
+				"txt-secondary": "rgba(var(--clr-txt-secondary))",
+				"accent-primary": "rgba(var(--clr-accent-primary))",
+				"accent-secondary": "rgba(var(--clr-accent-secondary))",
 			},
 		},
 	},
