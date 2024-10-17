62b2e023d580e1013a3263a66c1526933a86c2b4
MoatasemGamal <moatasemco@gmail.com>
2024-10-18 00:25:02 +0300

    add all static pages to vite builder

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
-export default {
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
 	base: "/minimalBlogUsingTailwindcss/",
-};
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				single: resolve(__dirname, "single.html"),
+				tags: resolve(__dirname, "tags.html"),
+				blog: resolve(__dirname, "blog.html"),
+			},
+		},
+	},
+});
